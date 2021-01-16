852f3ca76608ba31fe7b7836e7c7e56dd3f8a858
vira-tresyana <71061869+vira-tresyana@users.noreply.github.com>
2021-01-16 09:00:04 +0700

    Initial commit

---
+++ system/app/Models/Traits/Attributes/ProdukAttributes.php
@@ -0,0 +1,41 @@
+<?php 
+namespace App\Models\Traits\Attributes;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+trait ProdukAttributes {
+
+
+
+	function getHargaAttribute(){
+		return  "Rp.".number_format($this->attributes['harga']);
+	}
+
+	function handleUploadFoto(){
+		$this->handleDelete();
+		if(request()->hasFile('foto')){
+			$foto = request()->file('foto');
+			$destination = "images/produk";
+			$randomStr = Str::random(5);
+			$filename = $this->id."-".time()."-".$randomStr.".".$foto->extension();
+			$url = $foto->storeAs($destination, $filename);
+			$this->foto = "app/".$url;
+			$this->save();
+		}
+	}
+
+	function handleDelete(){
+		$foto = $this->foto;
+		$path = public_path($foto);
+		if(file_exists($path)){
+		
+			
+		}
+		return true;
+	}
+
+}
+
+
+ ?>
